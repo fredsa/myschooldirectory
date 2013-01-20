450345d8c18f4f269c1baada12b345010bc48e65
Fred Sauer <fredsa@google.com>
2013-01-20 11:21:51 -0800

    include missing last_name field

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from endpoints_proto_datastore.ndb import EndpointsModel
 class ParentGuardian(EndpointsModel):
 
    # adding 'id' gives us key.id()
-   _message_fields_schema = ('first_name', 'created', 'id')
+   _message_fields_schema = ('first_name', 'last_name', 'created', 'id')
 
    first_name = ndb.StringProperty(indexed=False)
    last_name = ndb.StringProperty(indexed=False)
