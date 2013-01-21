1f5da0eb84ec8f3fc85a79bb83e4e48327bb1b17
Fred Sauer <fredsa@google.com>
2013-01-21 15:33:29 -0800

    add email address

--- model.py
+++ model.py
@@ -6,12 +6,13 @@ from endpoints_proto_datastore.ndb import EndpointsModel
 class ParentGuardian(EndpointsModel):
 
    # adding 'id' gives us key.id()
-   _message_fields_schema = ('first_name', 'last_name', 'created', 'id')
+   _message_fields_schema = ('email', 'first_name', 'last_name', 'created', 'id')
 
+   email = ndb.StringProperty()
    first_name = ndb.StringProperty(indexed=False)
    last_name = ndb.StringProperty(indexed=False)
    created = ndb.DateTimeProperty(indexed=False, auto_now_add=True)
 
 
 # TODO: remove temporary development hack
-ParentGuardian.get_or_insert('42', first_name='Jane', last_name='Smith')
+ParentGuardian.get_or_insert('42', email='test@example.com', first_name='Jane', last_name='Smith')
