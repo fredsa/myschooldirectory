56dde11eb548ffe4c7a4f994ef1273600bf916a9
Fred Sauer <fredsa@google.com>
2013-01-24 11:05:23 -0800

    add progress bar

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -12,37 +12,54 @@ function PageController($scope, $http, $log, $window, $timeout) {
   var SCOPES = [SCOPE_EMAIL, SCOPE_USERINFO];
   //var SCOPES = ['https://www.googleapis.com/auth/userinfo.email'];
 
-  // called by the HTML page once our API client is ready
-  $window.gapi_ready = function() {
-    $log.log('1. gapi_ready()');
-    $scope.api_ready = true;
-    $scope.$apply();
-    load_apis();
+  var LOAD_PROGRESS_STATES = [
+    [10, 'Loading JavaScript client'],
+    [20, 'Loading APIs'],
+    [40, 'Authorizing'],
+    [60, 'Signing in'],
+    [80, 'Accessing API'],
+    [100, 'Done'],
+  ];
+
+  function set_progress(step, msg) {
+    var state = LOAD_PROGRESS_STATES[step];
+    $scope.progress_percent = state[0];
+    $scope.progress_msg = state[1] + '...';
+    //$log.log(state);
   }
+
+  set_progress(0);
+
+  // called by the HTML page once our API client is ready
+  $window.gapi_ready = function() { $scope.$apply(function() {
+    $scope.api_ready = true;
+    load_apis();
+  })};
 
   function fatal_error(err) {
     $scope.fatal_error = err;
-    $scope.$apply();
   }
 
   function load_apis() {
+    set_progress(1);
 
     var apis_to_load = 0;
 
-    function callback(load_resp) {
-      $log.log('3. gapi.client.load() -> ', load_resp);
+    function callback(load_resp) { $scope.$apply(function() {
+      set_progress(2);
+      $log.log('gapi.client.load() -> ', load_resp);
       if (load_resp) {
         fatal_error(load_resp);
         return;
       }
       if (--apis_to_load == 0) {
-        $scope.authorize(true);
+        authorize(true);
       }
-    }
+    }) };
 
     function load() {
       apis_to_load++;
-      $log.log('2. gapi.client.load(' + arguments[0] + ', ' + arguments[1] + ', ...)');
+      //$log.log('gapi.client.load(' + arguments[0] + ', ' + arguments[1] + ', ...)');
       gapi.client.load.apply(this, arguments);
     }
 
@@ -50,66 +67,66 @@ function PageController($scope, $http, $log, $window, $timeout) {
     load('directory', 'v1', callback, ROOT);
   }
 
-  $scope.authorize = function(immediate) {
+  function authorize(immediate) {
+    set_progress(3);
 
-    //gapi.client.setApiKey('AIzaSyA-RaSQdn4FPG7luv2dqUnmkk8B39CqlrQ');
     gapi.auth.authorize({
         client_id: CLIENT_ID,
         scope: SCOPES,
         immediate: immediate,
         response_type: 'token id_token',
-    }, function(authResp) {
-      // {state: "", access_token: "ya29.AHESR2wsf7CTl4B6_1h",
-      //  token_type: "Bearer", expires_in: "3600",
-      //  id_token: "eyJhbGciOi8WKIvVhSD70bmFpCeQDXscxPKwN-AssQPTR8"}
-      $log.log('4. gapi.auth.authorize(immediate=' + immediate + ') -> ', authResp);
+    }, function(authResp) { $scope.$apply(function() {
+      $log.log('gapi.auth.authorize(immediate=' + immediate + ') -> ', authResp);
       if (!authResp || authResp.error) {
         $scope.authfail = true;
-        $scope.$apply();
         // abort
         return;
       }
 
-      gapi.client.oauth2.userinfo.get().execute(function(userinfo) {
-        $log.log('5. gapi.client.oauth2.userinfo.get() -> ', userinfo);
-        if (userinfo.code) {
-          if (immediate) {
-            $scope.authorize(false);
-          }
-          return;
-        }
-        if (userinfo.verified_email) {
-          $scope.email = userinfo.email;
-        }
-        var token = gapi.auth.getToken();
-        //$log.log('token=', token);
-        token.access_token = token.id_token;
-        //$log.log('gapi.auth.setToken(', token, ')');
-        gapi.auth.setToken(token);
-
-        use_api();
-      });
-
-     });
+      set_progress(4);
+      get_user();
+    }) });
 
   };
 
+  function get_user() {
+    gapi.client.oauth2.userinfo.get().execute(function(userinfo) { $scope.$apply(function() {
+      $log.log('gapi.client.oauth2.userinfo.get() -> ', userinfo);
+      if (userinfo.code) {
+        if (immediate) {
+          authorize(false);
+        }
+        return;
+      }
+      if (userinfo.verified_email) {
+        $scope.email = userinfo.email;
+      }
+      var token = gapi.auth.getToken();
+      //$log.log('token=', token);
+      token.access_token = token.id_token;
+      //$log.log('gapi.auth.setToken(', token, ')');
+      gapi.auth.setToken(token);
+
+      use_api();
+    }) });
+  }
+
   function use_api() {
+    set_progress(5);
     gapi.client.directory.parentguardian.list()
-    .execute(function(apiResp) {
-      $log.log('6. gapi.client.directory.parentguardian.list() -> ', apiResp);
+    .execute(function(apiResp) { $scope.$apply(function() {
+      $log.log('gapi.client.directory.parentguardian.list() -> ', apiResp);
       if (apiResp.state || apiResp.error_message) {
         fatal_error(apiResp);
         return;
       }
 
       $scope.parents = apiResp.items;
-      $scope.$apply();
-    });
+    }) });
   }
 
   $scope.signin = function() {
-    $scope.authorize(false);
+    authorize(false);
   }
 
   $scope.get_config = function() {
