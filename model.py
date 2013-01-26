d3541f176d240cc191b02bca9ede06311ba3ee73
Fred Sauer <fredsa@google.com>
2013-01-26 12:17:49 -0800

    workaround _message_fields_schema issue http://code.google.com/p/endpoints-proto-datastore/issues/detail?id=38

--- model.py
+++ model.py
@@ -4,20 +4,18 @@ from endpoints_proto_datastore.ndb import EndpointsModel
 
 
 class Person(EndpointsModel):
-
-   # adding 'id' gives us key.id()
-   _message_fields_schema = ('email', 'first_name', 'last_name', 'created', 'id')
-
    email = ndb.StringProperty()
    first_name = ndb.StringProperty(indexed=False)
    last_name = ndb.StringProperty(indexed=False)
    created = ndb.DateTimeProperty(indexed=False, auto_now_add=True)
 
 class ParentGuardian(Person):
-  pass
+   # adding 'id' gives us key.id()
+   _message_fields_schema = ('email', 'first_name', 'last_name', 'created', 'id')
 
 class Child(Person):
-  pass
+   # adding 'id' gives us key.id()
+   _message_fields_schema = ('email', 'first_name', 'last_name', 'created', 'id')
 
 
 # TODO: remove temporary development hack
